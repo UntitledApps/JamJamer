b6fa213072c0ccc3975a644a014b823db7aa011c
Lenny Dirnhofer <lenny.dirnhofer@icloud.com>
2024-01-29 14:59:37 +0100

    Scene Setup

--- JamJam/Assets/-Scripts/PlayerBehav.cs
+++ JamJam/Assets/-Scripts/PlayerBehav.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector3 = UnityEngine.Vector3;
+using Vector2 = UnityEngine.Vector2;
 
-public class PlayerBehav : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed, hookPower, mouseSens; 
+    [SerializeField] private Gameobject Cam; 
 
-    void Start()
+    private Rigidbody rb;
+
+    private Vector3 moveVec;
+    private Vector2 mouseInput;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-
-    void Update()
+    private void OnMovement(InputValue value)
     {
+        moveVec =  value.Get<Vector2>() * moveSpeed;
+    }
+    private void OnLook(InputValue value)
+    {
+        mouseInput = value.Get<Vector2>();
         
+        Cam.transform.Rotate(-mouseInput.y * mouseSens, mouseInput.x * mouseSens, 0);
+        
+        //maybe implement a way to rotate the player on its z axis ?  
+    }
+    private void OnJump()
+    {
+        rb.AddForce(transform.forward * dashPower);
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * moveVec.y + transform.right * moveVec.x);
     }
 }
