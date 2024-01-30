17785e089b7035309636aacd446bf7ce145f8a3c
UntitledApps <neu220361@spengergasse.at>
2024-01-30 09:06:23 +0100

    Updated Cricket Spawner

--- JamJam/Assets/CricketSpawner.cs
+++ JamJam/Assets/CricketSpawner.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CricketSpawner : MonoBehaviour
 {
+    
+    [SerializeField] private GameObject cricketPrefab;
+    [SerializeField] private bool hasSpawned = false;
+    [SerializeField] private float spawnDelay = 1.5f;
+    [SerializeField] private int howManyToSpawn = 5;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void SpawnCricket()
     {
-        
+        Instantiate(cricketPrefab, transform.position, Quaternion.identity);
+    }
+
+    Vector3 randomPosition()
+    {
+         return transform.position + new Vector3(UnityEngine.Random.Range(-5, 5), transform.position.y + 5, UnityEngine.Random.Range(-5, 5));
+    }
+
+    void Spawn()
+    {
+        for(int i = 0; i < howManyToSpawn; i++)
+        {
+            Invoke(nameof(SpawnCricket), spawnDelay);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+       if(other.gameObject.CompareTag("Player"))
+       {
+           if(!hasSpawned)
+           {
+
+               Spawn();
+               hasSpawned = true;
+
+
+           }
+       }
     }
 }
