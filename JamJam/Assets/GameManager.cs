f435916a550676838668d0ae19a5d56b615e8c6a
Lenny Dirnhofer <lenny.dirnhofer@icloud.com>
2024-01-30 14:50:15 +0100

    CU

--- JamJam/Assets/GameManager.cs
+++ JamJam/Assets/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private int maxIslands;
-    private int maxIslands;
+    private List<GameObject> maxIslands = new List<GameObject>;
     
     
 
