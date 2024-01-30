dda42b3b3959f6ffbf25f2be838b876cd2afde28
Lenny Dirnhofer <lenny.dirnhofer@icloud.com>
2024-01-30 14:47:23 +0100

    IslandWork

---
+++ JamJam/Assets/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int maxIslands;
+    private int maxIslands;
+    
+    
+
+}
