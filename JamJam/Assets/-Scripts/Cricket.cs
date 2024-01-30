bc8ddb7911ff8485254ab23d64f8871ddc4b8449
UntitledApps <neu220361@spengergasse.at>
2024-01-30 08:26:30 +0100

    Added Cricket

---
+++ JamJam/Assets/-Scripts/Cricket.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Cricket : MonoBehaviour
+{
+    
+    bool isOnGround = true;
+    
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float runSpeed = 4f;
+    [SerializeField] private Transform player;
+    
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, runSpeed);
+
+    }
+
+    private void OnMouseDown()
+    {
+            
+            if (isOnGround)
+            {
+                jumpInTheAir();
+            }   
+    }
+
+
+    void jumpInTheAir()
+    {
+        isOnGround = false;
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            if(!isOnGround)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        
+        
+        
+       
+    }
+}
